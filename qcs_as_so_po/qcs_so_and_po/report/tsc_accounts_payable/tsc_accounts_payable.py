d96a60cec64579b77daa9da1d2763b486887aeb0
hiraniya <hiraniyas@gmail.com>
2024-05-15 09:08:39 +0000

    tsc payable update

--- qcs_as_so_po/qcs_so_and_po/report/tsc_accounts_payable/tsc_accounts_payable.py
+++ qcs_as_so_po/qcs_so_and_po/report/tsc_accounts_payable/tsc_accounts_payable.py
@@ -47,91 +47,123 @@ def get_columns(filters):
 			"label": ("Credit Note"),
 			"fieldtype": "Currency",
 		},
+		# {
+		# 	"fieldname": "po_outstanding",
+		# 	"label": ("PO Outstanding"),
+		# 	"fieldtype": "Currency",
+		# },
+		# {
+		# 	"fieldname": "invoiced_outstanding",
+		# 	"label": ("Invoiced Outstanding"),
+		# 	"fieldtype": "Currency",
+		# },
 		{
-			"fieldname": "po_outstanding",
-			"label": ("PO Outstanding"),
-			"fieldtype": "Currency",
-		},
-		{
-			"fieldname": "invoiced_outstanding",
-			"label": ("Invoiced Outstanding"),
+			"fieldname": "final_payable",
+			"label": ("Final Payable"),
 			"fieldtype": "Currency",
 		},
 	]
 	return columns
 
 
+
 def get_data(filters):
 	data = []
-	query_filters = []
-	query_filters.append(["company", "=", filters.get("company")])
-	query_filters.append(["transaction_date", ">=", filters.get("from_date")])
-	query_filters.append(["transaction_date", "<=", filters.get("to_date")])
+
+	query_filters_c = []
 	if filters.get("supplier"):
-		query_filters.append(["supplier", "=", filters.get("supplier")])
-	query_filters.append(["docstatus", "=", 1])
+		query_filters_c.append(["name", "=", filters.get("supplier")])
+	query_filters_c.append(["disabled", "=", 0])
 
-	doc = frappe.get_all("Purchase Order", filters=query_filters, fields=["supplier", "sum(grand_total) as grand_total", "sum(advance_paid) as advance_paid"], group_by="supplier")
-	if (doc):
-		for i in doc:
-			cus_data = {"supplier": i.get("supplier"), "purchase_order_amount": i.get("grand_total"), "advance_amount": i.get("advance_paid")}
+	supplier = frappe.get_all("Supplier", filters=query_filters_c, fields=["name"])
+	for k in supplier:
+		sup = k.get("name")
+		sup_data = {"supplier": sup}
 
-			query_filters1 = []
-			query_filters1.append(["company", "=", filters.get("company")])
-			query_filters1.append(["posting_date", ">=", filters.get("from_date")])
-			query_filters1.append(["posting_date", "<=", filters.get("to_date")])
-			query_filters1.append(["supplier", "=", i.get("supplier")])
-			query_filters1.append(["docstatus", "=", 1])
-   
-			pi_amount=[]
-			pi = frappe.get_all("Purchase Invoice", filters=query_filters1, fields=["supplier", "sum(grand_total) as grand_total"], group_by="supplier")
-			if pi:
-				for j in pi:
-					cus_data["invoiced_amount"] = j.get("grand_total")
-					pi_amount.append(j.get("grand_total"))
-			else:
-				cus_data["invoiced_amount"] = 0
-				pi_amount.append(0)
-    
-			query_filters2 = []
-			query_filters2.append(["company", "=", filters.get("company")])
-			query_filters2.append(["posting_date", ">=", filters.get("from_date")])
-			query_filters2.append(["posting_date", "<=", filters.get("to_date")])
-			query_filters2.append(["supplier", "=", i.get("supplier")])
-			query_filters2.append(["docstatus", "=", 1])
-			query_filters2.append(["status", "=", "Return"])
-   
-			pi_return = frappe.get_all("Purchase Invoice", filters=query_filters2, fields=["supplier", "sum(grand_total) as grand_total"], group_by="supplier")
-			if pi_return:
-				for j1 in pi_return:
-					cus_data["credit_amount"] = -(j1.get("grand_total"))
-			else:
-				cus_data["credit_amount"] = 0
-    
-			query_filters3 = []
-			query_filters3.append(["company", "=", filters.get("company")])
-			query_filters3.append(["posting_date", ">=", filters.get("from_date")])
-			query_filters3.append(["posting_date", "<=", filters.get("to_date")])
-			query_filters3.append(["party_type", "=", "Supplier"])
-			query_filters3.append(["party", "=", i.get("supplier")])
-			query_filters3.append(["docstatus", "=", 1])
-   
-			pay_amount=[]
-			pay = frappe.get_all("Payment Entry", filters=query_filters3, fields=["party", "sum(paid_amount) as paid_amount"], group_by="party")
-			if pay:
-				for p in pay:
-					cus_data["paid_amount"] = p.get("paid_amount")
-					pay_amount.append(p.get("paid_amount"))
-			else:
-				cus_data["paid_amount"] = 0
-				pay_amount.append(0)
-    
-			cus_data["invoiced_outstanding"] = pi_amount[0] - pay_amount[0]
-			cus_data["po_outstanding"] = i.get("grand_total") - pay_amount[0]
+		query_filters0 = []
+		query_filters0.append(["company", "=", filters.get("company")])
+		query_filters0.append(["transaction_date", ">=", filters.get("from_date")])
+		query_filters0.append(["transaction_date", "<=", filters.get("to_date")])
+		query_filters0.append(["status", "in", ["To Receive and Bill", "To Bill", "On Hold"]])
+		query_filters0.append(["supplier", "=", sup])
+		query_filters0.append(["docstatus", "=", 1])
+	
+		advance = [0]
+		po_amount_based_on_perbilled = [0]
+		po_ind = frappe.get_all("Purchase Order", filters=query_filters0, fields=["supplier", "grand_total", "per_billed", "advance_paid"])
+		if po_ind:
+			
+			for po in po_ind:
+				advance.append(po.get("advance_paid"))
+				po_grand_amount = po.get("grand_total") * (1 - po.get("per_billed") / 100)
+				po_amount_based_on_perbilled.append(po_grand_amount)
+		sup_data["advance_amount"] = sum(advance)
+		sup_data["purchase_order_amount"] = sum(po_amount_based_on_perbilled)
 
-			data.append(cus_data)
-					
+# Purchase invoice
+
+		query_filters1 = []
+		query_filters1.append(["company", "=", filters.get("company")])
+		query_filters1.append(["posting_date", ">=", filters.get("from_date")])
+		query_filters1.append(["posting_date", "<=", filters.get("to_date")])
+		query_filters1.append(["supplier", "=", sup])
+		query_filters1.append(["docstatus", "=", 1])
+
+		inv_amount=[]
+		pi = frappe.get_all("Purchase Invoice", filters=query_filters1, fields=["supplier", "sum(grand_total) as grand_total"], group_by="supplier")
+		if pi:
+			for j in pi:
+				sup_data["invoiced_amount"] = j.get("grand_total")
+				inv_amount.append(j.get("grand_total"))
+		else:
+			sup_data["invoiced_amount"] = 0
+			inv_amount.append(0)
+	#  return 
+	
+		query_filters2 = []
+		query_filters2.append(["company", "=", filters.get("company")])
+		query_filters2.append(["posting_date", ">=", filters.get("from_date")])
+		query_filters2.append(["posting_date", "<=", filters.get("to_date")])
+		query_filters2.append(["supplier", "=", sup])
+		query_filters2.append(["docstatus", "=", 1])
+		query_filters2.append(["status", "=", "Return"])
+
+		pi_return = frappe.get_all("Purchase Invoice", filters=query_filters2, fields=["supplier", "sum(grand_total) as grand_total"], group_by="supplier")
+		if pi_return:
+			for j1 in pi_return:
+				sup_data["credit_amount"] = -(j1.get("grand_total"))
+		else:
+			sup_data["credit_amount"] = 0
+   
+# payment entry
+
+		query_filters3 = []
+		query_filters3.append(["company", "=", filters.get("company")])
+		query_filters3.append(["posting_date", ">=", filters.get("from_date")])
+		query_filters3.append(["posting_date", "<=", filters.get("to_date")])
+		query_filters3.append(["party_type", "=", "supplier"])
+		query_filters3.append(["party", "=", sup])
+		query_filters3.append(["docstatus", "=", 1])
+
+		pay_amount=[]
+		pay = frappe.get_all("Payment Entry", filters=query_filters3, fields=["party", "sum(paid_amount) as paid_amount"], group_by="party")
+		if pay:
+			for p in pay:
+				sup_data["paid_amount"] = p.get("paid_amount")
+				pay_amount.append(p.get("paid_amount"))
+		else:
+			sup_data["paid_amount"] = 0
+			pay_amount.append(0)
+# Totals
+
+		sup_data["invoiced_outstanding"] = inv_amount[0] - pay_amount[0]
+		# sup_data["so_outstanding"] = sum(po_amount_based_on_perbilled) - pay_amount[0]
+		sup_data["final_payable"] = sup_data["purchase_order_amount"] + sup_data["invoiced_amount"] - sup_data["credit_amount"] - sup_data["paid_amount"]
+		
+		data.append(sup_data)
+			
 	return data
+		
 		
  
 	
